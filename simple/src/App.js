6e56d2a8a6a12abceb75d2041c516402a7934e65
NIk <64087311+Nikolay-962@users.noreply.github.com>
2026-03-28 22:20:46 +0300

    second

--- simple/src/App.js
+++ simple/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Buttons from "./components/Buttons"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='smile__wrap'>
+      <h1> как вам зарплата</h1>
+      <div className= "text" id='one'></div>
+      <Buttons/>
+      </div>
     </div>
   );
 }
---
+++ simple/src/components/Buttons.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+
+const Buttons =()=> {
+   const hendelLeft =()=> {
+    document.getElementById('one').textContent="Мы даже и не сомневались что вам нравится";
+   }
+
+   const hendelRight =(EO)=> {
+    
+    const maxLeft =400;
+    const maxTop =-300;
+    const maxBottom =200;
+    EO.target.style.left = Math.random() * maxLeft - maxLeft/2 + 'px';
+    EO.target.style.top = Math.random() * maxTop + 'px';
+    EO.target.style.bottom = Math.random() * maxBottom + 'px';
+
+   }
+   
+    return (
+        <div className='buttons'>
+          <div className='buttons__wrap'>
+            
+              <button onMouseDown={hendelLeft} className='button'>Да</button>
+            
+          
+              <button  onMouseDown={hendelRight}  className='button '>Нет</button>
+                  
+          </div>
+        </div> 
+    )
+}
+
+export default Buttons;
